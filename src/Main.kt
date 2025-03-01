e57a4de2272ed22afccdfba9941667e9ea5b8c65
Shepora <alarconchavezluisalejandro@gmail.com>
2025-03-01 13:58:21 -0500

    Juego de apuestas

--- src/Main.kt
+++ src/Main.kt
@@ -1,35 +1,46 @@
+import kotlin.random.Random
 
 fun main() {
-    var IntentosPermitidos = 0
-    var CodigoAcceso: Int
-    var NombreVisitante: String
-    var MotivosVisita: String
+    var Dinero: Double
+    var NumeroDeLaSuerte: Int
+    var Opcion: String
 
-    while (IntentosPermitidos < 3){
-        println("Bienvenido, Por favor ingrese su codigo de acceso.")
-        CodigoAcceso = readln().toInt()
+    println("Por favor ingrese la cantidad que quiere apostar: ")
+    Dinero = readln().toDouble()
 
-        if (CodigoAcceso in 1000..1999){
-                print("Bienvenido, Empleado su ID es: $CodigoAcceso.")
+    if (Dinero <= 0) {
+        println("Por favor ingrese una cantidad valida para poder apostar")
+    } else {
+        while (Dinero > 0) {
+            NumeroDeLaSuerte = Random.nextInt(1, 4)
+            println("El numero de la suerte es: $NumeroDeLaSuerte")
+
+            if (NumeroDeLaSuerte == 3) {
+                print("Has perdido todo, !mejor suerte para la proxima!")
+                Dinero = 0.0
                 break
-            } else if (CodigoAcceso in 2000..2999){
-                println("Bienvenido Visitante, Ingrese su nombre por favor: ")
-            NombreVisitante = readln().toString()
+            } else if (NumeroDeLaSuerte == 2) {
+                Dinero /= 2
+                println("Has tenido suerte pero no todo salio bien, has perdido la mitad tu dinero. Te quedan: $Dinero ")
+                println("¿Quieres seguir jugando? (si/no): ")
+                Opcion = readln().lowercase()
 
-            println("Por favor ingrese el motivo de su visita: ")
-           MotivosVisita = readln().toString()
+                if (Opcion == "no") {
+                    println("Te retiras con: $ $Dinero. !Gracias por jugar!")
+                    break
+                }
+            } else if (NumeroDeLaSuerte == 1) {
+                Dinero *= 2
+                println("!Has Corrido Con Suerte. Haz ganado el doble de tu apuesta, ahora tienes: $ $Dinero")
+                println("Quieres seguir jugando? (si/no): ")
+                Opcion = readln().lowercase()
 
-            println("Acceso Autorizado, Visitante: $NombreVisitante, Motivo: $MotivosVisita")
-            break
-            }
-            else {
-            IntentosPermitidos++
-            println("Su codigo es incorrecto. Intento $IntentosPermitidos de 3")
-
-            if (IntentosPermitidos == 3) {
-                println("Su acceso ha sido Bloqueado")
-
+                if (Opcion == "no") {
+                    println("Te vas con: $ $Dinero. !Gracias por jugar!")
+                    break
+                }
             }
         }
     }
 }
+
